362bc250f35d58fef82bd5a8bcd19cb3b54420ef
Thomas Guillet <guillet.thomas@gmail.com>
2020-08-06 07:33:30 +0200

    Allow tests to run with pandas

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
             "autopep8 ==1.5",
             "flake8 >=3.5.0,<3.8.0",
             "flake8-print",
+            "pandas == 1.1.0",
             "pycodestyle >=2.3.0,<2.6.0",  # To avoid incompatibility with flake
             ]
         },
